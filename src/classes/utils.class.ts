8bb1f1ea640075fdd42295d93718cad4d911bc97
Anthony Ubillus <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2020-11-16 14:20:53 +0100

    Ya he implementado la opción de recortar una imagen

--- src/classes/utils.class.ts
+++ src/classes/utils.class.ts
@@ -1,17 +1,60 @@
 import { User } from "./user.class";
+import Cropper from "cropperjs";
 
 export class Utils {
     static loadImage(event: Event, image: HTMLImageElement): void {
         let file: File = (event.target as HTMLInputElement).files[0];
         let reader = new FileReader();
-
         if (file) reader.readAsDataURL(file);
 
         reader.addEventListener('load', e => {
             image.src = reader.result.toString();
+         
+           image.addEventListener('change',()=>{})
         });
 
 
     }
+
+    static loadImageCrop(event: Event, image: HTMLImageElement, cropper:Cropper,btnCrop:HTMLButtonElement, photo:HTMLImageElement): void {
+        let file: File = (event.target as HTMLInputElement).files[0];
+        let reader = new FileReader();
+        if (file) reader.readAsDataURL(file);
+
+        reader.addEventListener('load', e => {
+            image.src = reader.result.toString();
+            cropper.destroy();
+            
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                center: true,
+                viewMode:3,
+                dragMode: "crop",
+            
+                minContainerWidth: 200
+            });
+
+            btnCrop.addEventListener('click', ()=>{
+                let result:HTMLCanvasElement = cropper.getCroppedCanvas({
+                    width:400,
+                    height:400
+                });
+        
+                result.toBlob(function(blob){
+                    let url = URL.createObjectURL(blob);
+                    let reader : FileReader = new FileReader();
+                    reader.readAsDataURL(blob);
+        
+                    reader.addEventListener('load',()=>{
+                        photo.src = reader.result.toString();
+                    })
+                })
+            })
+        });
+
+
+    }
+   
+
 
 }
--- src/edit-profile.ts
+++ src/edit-profile.ts
@@ -4,35 +4,46 @@ import { User } from "./classes/user.class";
 import { Utils } from "./classes/utils.class";
 import { ResponseError } from "./interfaces/responses";
 
+import Cropper from 'cropperjs';
+
+
+
 let me: User;
 let formProfile: HTMLFormElement;
 let formPhoto: HTMLFormElement;
 let formPassword: HTMLFormElement;
-let logout:HTMLElement;
-let imgPreview:HTMLImageElement;
+let logout: HTMLElement;
+let cropper:Cropper;
+let imgPreview: HTMLImageElement;
+let btnCrop:HTMLButtonElement;
 
-
-Auth.checkToken().catch(()=>location.assign('login.html'));
+//Auth.checkToken().catch(()=>location.assign('login.html'));
 document.addEventListener('DOMContentLoaded', e => {
     formProfile = document.getElementById('form-profile') as HTMLFormElement;
     formPhoto = document.getElementById('form-photo') as HTMLFormElement;
     formPassword = document.getElementById('form-password') as HTMLFormElement;
-    imgPreview = document.getElementById('imgPreview')as HTMLImageElement;
+    imgPreview = document.getElementById('imgPreview') as HTMLImageElement;
+    btnCrop = document.getElementById('crop') as HTMLButtonElement;
     logout = document.getElementById('logout');
-    logout.addEventListener('click',Auth.logout);
+    logout.addEventListener('click', Auth.logout);
 
     User.getProfile().then(x => {
         me = new User(x.user);
         completeForm();
     });
 
-    formProfile.addEventListener('submit',updateProfile);
-    formPassword.addEventListener('submit',updatePassword);
-    (formPhoto.image as HTMLInputElement).addEventListener('change', e=>{
+    formProfile.addEventListener('submit', updateProfile);
+    formPassword.addEventListener('submit', updatePassword);
+   
+    (formPhoto.image as HTMLInputElement).addEventListener('change', e => {
         imgPreview.classList.remove('d-none');
-        Utils.loadImage(e, imgPreview)
+        cropper = new Cropper(imgPreview);
+        Utils.loadImageCrop(e, imgPreview,cropper, btnCrop,(formPhoto.photo as HTMLImageElement));
+        
     });
-    formPhoto.addEventListener('submit',updateAvatar)
+
+  
+    formPhoto.addEventListener('submit', updateAvatar);
 
 })
 
@@ -43,66 +54,66 @@ function completeForm(): void {
 }
 
 
-function updateProfile(event:Event): void {
+function updateProfile(event: Event): void {
     event.preventDefault();
-    User.saveProfile((formProfile.nameUser as HTMLInputElement).value,(formProfile.email as HTMLInputElement).value).then(()=>{
+    User.saveProfile((formProfile.nameUser as HTMLInputElement).value, (formProfile.email as HTMLInputElement).value).then(() => {
         Swal.fire({
-            icon:'success',
-            title:'Profile Update',
-            text:'Name or email change successfull'
+            icon: 'success',
+            title: 'Profile Update',
+            text: 'Name or email change successfull'
         });
-    }).catch(x =>{
+    }).catch(x => {
         let promise: Promise<ResponseError> = x.json() as Promise<ResponseError>;
-        promise.then(x=>{
+        promise.then(x => {
             Swal.fire({
-                icon:'error',
-                title:'Update Profile Error',
-                text:x.message.join('\n')
+                icon: 'error',
+                title: 'Update Profile Error',
+                text: x.message.join('\n')
             });
         })
-       
-        
+
+
     })
 
 }
 
-function updatePassword(event:Event):void{
+function updatePassword(event: Event): void {
     event.preventDefault();
-    if(matchPassword()){
-        User.savePassword((formPassword.password as HTMLInputElement).value).then(()=>{
+    if (matchPassword()) {
+        User.savePassword((formPassword.password as HTMLInputElement).value).then(() => {
             formPassword.reset();
             Swal.fire({
-                icon:'success',
-                title:'Password Update',
-                text:'The password has changed update successfull'
+                icon: 'success',
+                title: 'Password Update',
+                text: 'The password has changed update successfull'
             });
         })
-    }else{
+    } else {
         Swal.fire({
-            icon:'error',
-            title:'Password error',
-            text:"The passwords doesn't match"
+            icon: 'error',
+            title: 'Password error',
+            text: "The passwords doesn't match"
         });
     }
 }
 
-function updateAvatar(event:Event):void{
+function updateAvatar(event: Event): void {
     event.preventDefault();
-    User.saveAvatar(imgPreview.src).then(x=>{
+    User.saveAvatar(imgPreview.src).then(x => {
         imgPreview.classList.add('d-none');
         (formPhoto.photo as HTMLImageElement).src = x;
         Swal.fire({
-            icon:'success',
-            title:'Photo Update',
-            text:"The photo's profile has changed update successfull"
+            icon: 'success',
+            title: 'Photo Update',
+            text: "The photo's profile has changed update successfull"
         });
     })
-    
+
 }
 
 
-function matchPassword():boolean{
-    if((formPassword.password as HTMLInputElement).value === (formPassword.password2 as HTMLInputElement).value)
+function matchPassword(): boolean {
+    if ((formPassword.password as HTMLInputElement).value === (formPassword.password2 as HTMLInputElement).value)
         return true;
     return false;
 }
