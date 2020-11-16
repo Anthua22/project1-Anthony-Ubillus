c6def23a5419dcbab2d7148169a0e9e709821b37
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2020-11-16 17:57:46 +0100

    Modifico la calse auth

--- src/classes/auth.class.ts
+++ src/classes/auth.class.ts
@@ -2,29 +2,27 @@ import { SERVER } from "../constants";
 import { IUser } from "../interfaces/iuser";
 import { TokenResponse } from "../interfaces/responses";
 import { Http } from "./http.class";
-import { User } from "./user.class";
+
 
 export class Auth {
 
-  
-
-    static async checkToken(): Promise<void>{
+    static async checkToken(): Promise<void> {
         let token = localStorage.getItem('token');
-        if(token){
-            try{
+        if (token) {
+            try {
                 await Http.get(`${SERVER}/auth/validate`);
-            }catch(e){
-                if(e.status && e.status===401){
+            } catch (e) {
+                if (e.status && e.status === 401) {
                     localStorage.removeItem('token');
                 }
                 throw new Error();
             }
-        }else{
+        } else {
             throw new Error();
         }
     }
 
-    static login(user:IUser): Promise<TokenResponse> {
+    static login(user: IUser): Promise<TokenResponse> {
         let peticion: Promise<TokenResponse> = Http.post(SERVER + "/auth/login", user);
         return peticion;
     }
